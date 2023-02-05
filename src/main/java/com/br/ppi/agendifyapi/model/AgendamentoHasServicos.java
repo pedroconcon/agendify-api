b6cb3a7583dc622fd46c23e2b06d267a5f505541
Pedro Concon <pedroconcon1309@gmail.com>
2023-02-05 17:18:04 -0300

    add agendamento has services

--- src/main/java/com/br/ppi/agendifyapi/model/AgendamentoHasServicos.java
+++ src/main/java/com/br/ppi/agendifyapi/model/AgendamentoHasServicos.java
@@ -1,4 +1,21 @@
 package com.br.ppi.agendifyapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+@Builder
 public class AgendamentoHasServicos {
+    @Column(name="id_profissional")
+    private Long idProfissional;
+    @Column(name = "id_servicos")
+    private Long idSevicos;
 }
